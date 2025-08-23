e7e68d9868ecd4648409b9a5a761b7e0748c8d6a
Liangyu Wu <133572075+Liangyu5wu@users.noreply.github.com>
2025-08-22 22:59:31 -0700

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,7 @@
 
 from .transformer_layers import PositionalEncoding, MultiHeadSelfAttention, TransformerBlock
 from .transformer_model import TransformerModel
+from .dnn_model import DNNModel, MaskedAttentionPooling
 
-__all__ = ['PositionalEncoding', 'MultiHeadSelfAttention', 'TransformerBlock', 'TransformerModel']
+__all__ = ['PositionalEncoding', 'MultiHeadSelfAttention', 'TransformerBlock', 'TransformerModel',
+           'DNNModel', 'MaskedAttentionPooling']
